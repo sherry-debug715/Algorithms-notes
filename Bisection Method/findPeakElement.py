5614cdd7290ca43463a2004c8b7a92efd35234b8
sherryyu <bambooyu715@gmail.com>
2023-10-03 08:15:35 -0400

    UPDATING NOTES FOR bisection method: added problem link to maximum subtree and updated problem find peak element

---
+++ Bisection Method/findPeakElement.py
@@ -0,0 +1,30 @@
+"""
+Lintcode problem 75: https://www.lintcode.com/problem/75/?fromId=161&_from=collection
+"""
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param a: An integers array.
+    @return: return any of peek positions.
+    """
+    def find_peak(self, a: List[int]) -> int:
+        start, end = 0, len(a) - 1 
+
+        while start + 1 < end:
+            mid = (start + end) // 2 
+            right = mid + 1 
+            if a[mid] < a[right]:
+                start = mid 
+            elif a[mid] > a[right]:
+                end = mid 
+            else:
+                if a[left] < a[mid] > a[right]:
+                    return mid 
+            
+        if a[start] > a[end]:
+            return start 
+        else:
+            return end 
---
+++ Bisection Method/maximumSubtree.py
@@ -0,0 +1,47 @@
+"""
+Lintcode problem 628: https://www.lintcode.com/problem/628/?fromId=161&_from=collection
+"""
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the root of binary tree
+    @return: the maximum weight node
+    """
+    # use a helper function: function returns a tuple: 
+    #   (cur_sum, max_sub_sum, root_of_subtree_with_max_sum) 
+    def find_subtree(self, root: TreeNode) -> TreeNode:
+        # write your code here
+        _, _, max_subtree_root = self._find_subtree(root) 
+        return max_subtree_root 
+    
+    def _find_subtree(self, root): 
+        # when traverse across leaf node, should hit this line 
+        if root is None:
+            return 0, float("-inf"), None 
+        
+        left_sum, left_max_sum, left_max_subtree = self._find_subtree(root.left) 
+        right_sum, right_max_sum, right_max_subtree = self._find_subtree(root.right) 
+
+        cur_sum = root.val + left_sum + right_sum 
+        cur_max = max(cur_sum, left_max_sum, right_max_sum) 
+
+        # decide which subtree has the largest sum 
+        # first condition, when we are at leaf nodes, this situation apply
+        if cur_max == cur_sum:
+            return (cur_sum, cur_max, root) 
+        elif cur_max == left_max_sum:
+            return (cur_sum, cur_max, left_max_subtree) 
+        else:
+            return (cur_sum, cur_max, right_max_subtree)
+
