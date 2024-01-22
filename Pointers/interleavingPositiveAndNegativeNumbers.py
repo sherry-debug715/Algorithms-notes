86bd8f88664a44f3d1871c32b278215a33136e5e
sherryyu <bambooyu715@gmail.com>
2024-01-22 15:57:18 -0500

    update notes for interleaving positive and negative numbers problem

--- Pointers/interleavingPositiveAndNegativeNumbers.py
+++ Pointers/interleavingPositiveAndNegativeNumbers.py
@@ -1,11 +1,12 @@
 """
 Lintcode 144: https://www.lintcode.com/problem/144/?fromId=161&_from=collection
 """
-
+# Solution 1
 from typing import (
     List,
 )
-
+# Time: O(n)
+# Space: O(n)
 class Solution:
     """
     @param a: An integer array.
@@ -24,3 +25,50 @@ class Solution:
             a[::2], a[1::2] = neg, pos 
 
         return a
+
+class Solution:
+    """
+    @param: A: An integer array.
+    @return: nothing
+    """
+    def rerange(self, A):
+        
+        if A is None or len(A) <= 1:
+            return
+        
+        # Assume A does not contain 0
+        
+        lo, hi = 0, len(A)-1
+        
+        while lo <= hi:
+            
+            while lo <= hi and A[lo] < 0:
+                lo += 1
+                
+            while lo <= hi and A[hi] > 0:
+                hi -= 1
+                
+            if lo <= hi:
+                A[lo], A[hi] = A[hi], A[lo]
+                lo += 1
+                hi -= 1
+                
+        # A[:lo] are neg number (amout: lo)
+        # A[hi+1:] are pos number (amount: len(A)-hi-1)
+        
+        neg_count = lo
+        pos_count = len(A)-hi-1
+        
+        # the amount of pos and neg number should at most diff by one
+        if abs(neg_count - pos_count) > 1:
+            return
+        
+        lo = 1 if neg_count >= pos_count else 0
+        hi = len(A)-2 if pos_count >= neg_count else len(A)-1
+        
+        while lo < hi:
+
+            A[lo], A[hi] = A[hi], A[lo]
+            
+            lo += 2
+            hi -= 2
