f97493ae13ff9c2039f6e2f15ae85dc3ca86dfbe
sherryyu <bambooyu715@gmail.com>
2024-02-12 12:50:30 -0500

    added complexity analysis to problem subsets using dfs approach 1

--- DFS/Subsets/withoutDup/method1.py
+++ DFS/Subsets/withoutDup/method1.py
@@ -8,6 +8,9 @@ class Solution:
     @return: A list of lists
              we will sort your return value in output
     """
+    # Time: O(2^n) - each element in the input list has two choices: either to be included in the current subset or not, leading to a total of O(2^n) possible subsets for a list of "n" elements.
+    # Space: O(n) - the space complexity is to be considered from two main components: space used by the recursive call stack O(n) where n is the number of elements stored in input list, and the 2^n subsets stored in res.
+    
     def subsets(self, nums: List[int]) -> List[List[int]]:
         # write your code here
         res = [] 
