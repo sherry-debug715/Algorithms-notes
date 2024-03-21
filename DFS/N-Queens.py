dbe350060cb164a16b77f83bf3af6aad176c96e6
sherryyu <bambooyu715@gmail.com>
2024-03-21 12:15:06 -0400

    update optimated solution for problem n-queens

--- DFS/N-Queens.py
+++ DFS/N-Queens.py
@@ -10,32 +10,44 @@ class Solution:
     @return: All distinct solutions
              we will sort your return value in output
     """
+    # Time: O(Number of plans * N^2) where N^2 is the time to form a plan
     def solve_n_queens(self, n: int) -> List[List[str]]:
         if not n:
             return "" 
         
         res = []
-        self.search(res, n, [])
+        visited = {
+            "col": set(),
+            "sum": set(),
+            "diff": set()
+        }
+        self.search(res, n, [], visited)
         return res 
     
-    def search(self, res, n, cols):
-        row = len(cols) 
+    def search(self, res, n, cols, visited):
         if len(cols) == n:
             res.append(self.draw_board(cols, n))
             return 
-        
+
+        row = len(cols)
         for c in range(n):
-            if self.is_valid(row, c, cols):
+            if self.is_valid(c, cols, visited):
                 cols.append(c) 
-                self.search(res, n, cols)
+                visited["col"].add(c)
+                visited["sum"].add(row + c)
+                visited["diff"].add(row - c)
+                self.search(res, n, cols, visited)
                 cols.pop() 
+                visited["col"].remove(c)
+                visited["sum"].remove(row + c)
+                visited["diff"].remove(row - c)
     
-    def is_valid(self, r, c, cols):
-        for row, col in enumerate(cols):
-            if c == col:
-                return False 
-            if row - col == r - c or row + col == r + c:
-                return False 
+    def is_valid(self, c, cols, visited):
+        r = len(cols)
+        if c in visited["col"]:
+            return False 
+        if r - c in visited["diff"] or r + c in visited["sum"]:
+            return False 
         
         return True 
     
