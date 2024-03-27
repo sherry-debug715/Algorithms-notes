4d8ebd37b1ee2754ee0bf9ff67e756503636208d
sherryyu <bambooyu715@gmail.com>
2024-03-27 14:55:33 -0400

    update solution for problem pancake sorting

---
+++ Sorting/PancakeSorting.py
@@ -0,0 +1,43 @@
+# Lintcode problem 894: https://www.lintcode.com/problem/894/description
+
+from typing import (
+    List,
+)
+# Time: O(N^2)
+# Space: O(1)
+class Solution:
+    """
+    @param array: an integer array
+    @return: nothing
+    """
+    def pancake_sort(self, array: List[int]):
+        if not array:
+            return 
+        
+        for i in range(len(array) - 1, -1, -1):
+            max_idx = self.find_max_index(array, i)
+            if max_idx is None:
+                continue
+            FlipTool.flip(array, max_idx)
+            FlipTool.flip(array, i)
+
+        return array
+    
+    # given an index, the function should return the index of the max value from
+    # subarr arr[:idx] 
+    def find_max_index(self, arr, index):
+        max_idx = 0 
+        max_val = arr[index] 
+
+        for idx, val in enumerate(arr):
+            if idx == index:
+                break
+            if val > max_val:
+                max_val = val 
+                max_idx = idx 
+
+        if max_val == arr[index]:
+            return None
+        return max_idx
+
+
